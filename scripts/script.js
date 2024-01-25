d6084ee09c14e896396fc1eb618655b16adee57c
Njmin <gazinurnurtdinov35@gmail.com>
2024-01-25 05:28:34 +0300

    commit10

--- scripts/script.js
+++ scripts/script.js
@@ -3,4 +3,5 @@ let buy = document.getElementById("buy");
 
 buy.addEventListener("click", () => {
     document.getElementById("main").style.display = "none";
+    document.getElementById("first_name").style.display = "block";
 });
