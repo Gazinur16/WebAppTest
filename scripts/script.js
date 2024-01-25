81a47dc2eac9586460067d970c2e63b3754ae96c
Njmin <gazinurnurtdinov35@gmail.com>
2024-01-25 05:01:44 +0300

    commit7

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+<script>
+let tg = window.Telegram.WebApp;
+let buy = document.getElementById("buy");
+
+
+buy.addElementListener("click", () => {
+    document.getElementById("main").style.display = "none";
+});
+
+</script>
