68692b8c678cba399930ca810eceb4ff95c45832
Njmin <gazinurnurtdinov35@gmail.com>
2024-03-01 12:37:22 +0300

    commit12

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 TelegramWebApp.getUserProfilePhotos().then(function (data) {
     var file_id = data.photos[0][0].file_id;
     var img = document.getElementById('user-avatar');
-    img.src = 'https://api.telegram.org/file/botYOUR_BOT_TOKEN/' + file_id;
+    img.src = 'https://api.telegram.org/file/6834164465:AAEokQ3sNvwTYG8fewiqUp11_vVOZECUDBg/' + file_id;
   });
