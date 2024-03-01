2c90437ac8de782fd22c588c2e8add3209b9141c
Njmin <gazinurnurtdinov35@gmail.com>
2024-03-01 12:35:28 +0300

    commit11

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,5 @@
-let tg = window.Telegram.WebApp;
-let buy = document.getElementById("buy");
-
-buy.addEventListener("click", () => {
-    document.getElementById("main").style.display = "none";
-    document.getElementById("first_name").style.display = "block";
-});
+TelegramWebApp.getUserProfilePhotos().then(function (data) {
+    var file_id = data.photos[0][0].file_id;
+    var img = document.getElementById('user-avatar');
+    img.src = 'https://api.telegram.org/file/botYOUR_BOT_TOKEN/' + file_id;
+  });
